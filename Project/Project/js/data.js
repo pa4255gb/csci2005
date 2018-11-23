60e19807b9f33a61105ff64b8850bee193689842
TomMcDonaldCentury <45017971+TomMcDonaldCentury@users.noreply.github.com>
2018-11-23 17:02:47 -0600

    Add files via upload
    Changed some css, balanced the form input

---
+++ Project/Project/js/data.js
@@ -0,0 +1,13 @@
+var books = [{title: "Starting Out with C++ from Control Structures to Objects",
+              newPrice: 167.10,
+              usedPrice: 123.10,
+              key: "proBook"},
+              {title: "Calculus Single Variable",
+              newPrice: 188.75,
+              usedPrice: 141.60,
+              key: "calcBook"},
+              {title: "Physics for Scientists and Engineers",
+              newPrice: 161.25,
+              usedPrice: 120.95,
+              key: "physBook"}];
+var pageTitles = ["ProgrammingFundamentals", "SingleVariableCalculus1", "Physics1"];

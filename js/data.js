1d33815d523e52cee04111efdf293221bf3c18db
pa4255gb <epidemic99@hotmail.com>
2018-12-04 19:56:29 -0600

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,4 @@
+//Used for testing log in page
+var dummyAccountCreds = [{username: "Shelby", password: "cv8513vq"},
+                        {username: "Keith", password: "pa4255gb"},
+                        {username: "Tom", password: "wq3570is"}];
---
+++ js/logIn.js
@@ -0,0 +1,8 @@
+//Suggestions for logIn - Shelby
+//Validate username and password >>> data.js
+//Go to myAccount.html if creds match >>> use window.location.assign();
+//Put submitted username into local storage
+//if window.localStorage.getItem("user") != null
+//bypass logIn.html and go to myAccount.html
+//Code to populate myAccount.html with data from submitted user >>> might want to put this in a
+//separate js file.

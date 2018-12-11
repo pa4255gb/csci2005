817d08ceb8cceb8939ad58f00db4bd8628b064ed
Shelby Medlock <43891819+Sam25M@users.noreply.github.com>
2018-12-11 08:41:21 -0600

    Add files via upload

--- js/logIn.js
+++ js/logIn.js
@@ -1,30 +1,22 @@
-$(function(){
-  $("#loginBtn").on("click", function(){
-    var username = $("#username").val();
-    var password = $("#password").val();
-    var token = 1;
+window.addEventListener('submit', testLogin);
 
-    //for testing
-    //console.log("first:" + token);
-    //console.log("username:" + username);
-    //console.log("password:" + password);
+function testLogin(e){
+  e.preventDefault();
 
-    for(var i in authUsers){
-      //console.log('authUsers for loop');
-      if(username.toLowerCase() == authUsers[i].username && password.toLowerCase() == authUsers[i].password) {
-        token = 0;
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  var token = 1;
 
-        //for testing
-        //console.log("token:" + token);
-
-        window.localStorage.setItem("user", username);
-        alert(username + " Is logged in!");
-        window.location.assign("myAccount.html");
+  for(var i=0; i < authUsers.length; i++) {
+        if(username.toLowerCase() == authUsers[i].username && password.toLowerCase() == authUsers[i].password) {
+          token = 0;
+          window.localStorage.setItem("user", username);
+          alert(username + " Is logged in!");
+          window.location.assign("myAccount.html");
       }
     }
     if(token == 1){
       alert("You have entered an incorrect user name or password");
       document.getElementById("logInForm").reset();
     }
-  });
-});
+}
